705863bf271f16e7f68d94933a8487a88611f53e
robiulalamdev <robiulalam762400@gmail.com>
2024-10-17 15:19:41 +0600

    third section done

---
+++ src/components/Funnel/FunnelThirdSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import bg from "../../assets/images/funnel/thirdSection/bg.png";
+import img1 from "../../assets/images/funnel/thirdSection/img1.png";
+
+const FunnelThirdSection = () => {
+  return (
+    <div
+      className="min-h-[800px] pt-[41px] md:pt-[61px] pb-[100px] md:pb-[139px]"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="container">
+        <img
+          src={img1}
+          alt=""
+          className="max-w-[1185px] w-full object-contain mx-auto"
+        />
+        <button className="w-[216px] h-[69px] md:w-[288px] md:h-[93px] !border-b-[8px] hover:border-b-[9px] border-x-[4px] border-t-[2px] border-primary rounded-[8px] md:rounded-[13.573px] bg-wp hover:bg-wp/85 text-cmn font-italic text-[#0D0D0D] capitalize text-[20px] md:text-[24px] font-semibold leading-normal mt-[35px] md:mt-[45px] block mx-auto">
+          Start Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FunnelThirdSection;
--- src/pages/Funnel/Funnel.jsx
+++ src/pages/Funnel/Funnel.jsx
@@ -1,11 +1,13 @@
 import FunnelBanner from "../../components/Funnel/FunnelBanner";
 import FunnelSecondSection from "../../components/Funnel/FunnelSecondSection";
+import FunnelThirdSection from "../../components/Funnel/FunnelThirdSection";
 
 const Funnel = () => {
   return (
     <div className="bg-[#1D1D1D]">
       <FunnelBanner />
       <FunnelSecondSection />
+      <FunnelThirdSection />
     </div>
   );
 };
