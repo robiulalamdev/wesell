44aa488782fe86560b1dc2353aa94712c0ff03a7
robiulalamdev <robiulalam762400@gmail.com>
2024-10-29 12:21:26 +0600

    Update FunnelBanner component and images

--- src/components/Funnel/FunnelBanner.jsx
+++ src/components/Funnel/FunnelBanner.jsx
@@ -74,13 +74,28 @@ const FunnelBanner = () => {
 
   useEffect(() => {
     if (openModal) {
+      const scrollY = window.scrollY;
+
+      // Apply styles to lock scrolling
+      document.body.style.position = "fixed";
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = "100%";
       document.body.style.overflow = "hidden";
     } else {
+      // Remove styles and restore scroll position
+      const scrollY = document.body.style.top;
+      document.body.style.position = "";
+      document.body.style.top = "";
+      document.body.style.width = "";
       document.body.style.overflow = "auto";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
     }
 
-    // Clean up to reset overflow when component unmounts
+    // Cleanup to restore styles when component unmounts
     return () => {
+      document.body.style.position = "";
+      document.body.style.top = "";
+      document.body.style.width = "";
       document.body.style.overflow = "auto";
     };
   }, [openModal]);
